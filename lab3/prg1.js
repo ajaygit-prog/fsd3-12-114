c2cb269c4c521c79b06841bc9a31967bdbcd46ce
ajaygit-prog <ajayprakashmishra1001@gmail.com>
2026-09-18 14:41:56 +0530

    prg1 competed

--- lab3/prg1.js
+++ lab3/prg1.js
@@ -4,7 +4,7 @@ const server = http.createServer() ;
 
 server.on("request" , (req,res) => {
     res.write("Hello from Server") ;
-    res.end() ;
+    res.end("ended") ;
 }) ;
 server.listen(4444 , () => {
     console.log("server is running...") ;
